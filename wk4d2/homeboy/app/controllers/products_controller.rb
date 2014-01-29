cecec57337587b5b442004c54927ed8851c61a21
jongmini <jongmin.choi@gmail.com>
2014-01-29 10:01:42 -0800

    after lab review

--- wk4d2/homeboy/app/controllers/products_controller.rb
+++ wk4d2/homeboy/app/controllers/products_controller.rb
@@ -18,7 +18,7 @@ class ProductsController < ApplicationController
   	id = params[:id]
   	@product = Product.find(id)
   	@categories = @product.categories
-  	@all_categories = Category.all.each
+  	@all_category_names = Category.all.map {|cat| [cat.name]}
   end
 
   def update
@@ -40,11 +40,18 @@ class ProductsController < ApplicationController
   end
 
   def disconnect
-  	id = params[:id]
-  	cat = Category.find(id)
-  	raise params.inspect
+  	cat_id = params[:cat_id]
+  	prod_id = params[:id]
 
-  end
+  	product = Product.find(prod_id)
+  	category = Category.find(cat_id)
+
+  	product.categories.destroy(category)
+
+  	redirect_to product_path
+
+  	# Category.find(id).products.find(@product.id)
+	end
 
 
 
